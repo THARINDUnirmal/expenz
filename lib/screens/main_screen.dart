27ec34451b722f7c9cb73023e6cce938deb3309d
THARINDUnirmal <tharinduna0@gmail.com>
2024-09-19 00:02:19 -0700

    bottom navigation bar impliment

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,3 +1,4 @@
+import 'package:expenz_app/utils/colors.dart';
 import 'package:flutter/material.dart';
 
 class MainScreen extends StatefulWidget {
@@ -8,18 +9,67 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  //pages list
+
+  final List<Widget> pages = [];
+
+  //curunt index
+  int cIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "Main Page",
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.w600,
-          ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: cIndex,
+        selectedIconTheme: const IconThemeData(size: 30),
+        selectedItemColor: kMainColor,
+        selectedFontSize: 18,
+        selectedLabelStyle: const TextStyle(
+          fontWeight: FontWeight.w600,
         ),
+        unselectedItemColor: kGrey,
+        unselectedIconTheme: const IconThemeData(
+          size: 25,
+        ),
+        items: [
+          const BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+              ),
+              label: "Home"),
+          const BottomNavigationBarItem(
+              icon: Icon(
+                Icons.transcribe,
+              ),
+              label: "Transaction"),
+          BottomNavigationBarItem(
+              icon: Container(
+                padding: const EdgeInsets.all(20),
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: kMainColor,
+                ),
+                child: const Center(
+                  child: Icon(
+                    Icons.add,
+                    color: kWhite,
+                  ),
+                ),
+              ),
+              label: ""),
+          const BottomNavigationBarItem(
+              icon: Icon(
+                Icons.pie_chart,
+              ),
+              label: "Budget"),
+          const BottomNavigationBarItem(
+              icon: Icon(
+                Icons.person,
+              ),
+              label: "Profile"),
+        ],
+        type: BottomNavigationBarType.fixed,
       ),
+      //body: pages[cIndex],
     );
   }
 }
